6781a43cf9f10fe0d2295254a6384d671433d464
Shane <shane.morgan@itechpros.com.au>
2020-09-21 21:10:05 +1000

    Add files via upload
    Update CKEditor to 4.15.0.

--- src/webparts/fckText/FckText.module.scss.ts
+++ src/webparts/fckText/FckText.module.scss.ts
@@ -1,11 +1,13 @@
 /* tslint:disable */
 require('./FckText.module.css');
 const styles = {
-  carousel3D: 'carousel3D_189b4e86',
-  container: 'container_189b4e86',
-  row: 'row_189b4e86',
-  listItem: 'listItem_189b4e86',
-  button: 'button_189b4e86',
+  carousel3D: 'carousel3D_84541eea',
+  container: 'container_84541eea',
+  row: 'row_84541eea',
+  listItem: 'listItem_84541eea',
+  button: 'button_84541eea',
+  cke: 'cke_84541eea',
+  cke_top: 'cke_top_84541eea',
 };
 
 export default styles;
--- src/webparts/fckText/FckTextWebPart.ts
+++ src/webparts/fckText/FckTextWebPart.ts
@@ -10,138 +10,167 @@ import {
   IPropertyPaneConfiguration,
   IWebPartContext,
   PropertyPaneToggle
-} from '@microsoft/sp-webpart-base';
-import { DisplayMode, Version } from '@microsoft/sp-core-library';
-import { Environment, EnvironmentType } from '@microsoft/sp-core-library';
+} from "@microsoft/sp-webpart-base";
+import { DisplayMode, Version } from "@microsoft/sp-core-library";
+import { Environment, EnvironmentType } from "@microsoft/sp-core-library";
 
-import * as strings from 'fckTextStrings';
-import { IFckTextWebPartProps } from './IFckTextWebPartProps';
-import { SPComponentLoader } from '@microsoft/sp-loader';
+import * as strings from "fckTextStrings";
+import { IFckTextWebPartProps } from "./IFckTextWebPartProps";
+import { SPComponentLoader } from "@microsoft/sp-loader";
 
-export default class FckTextWebPart extends BaseClientSideWebPart<IFckTextWebPartProps> {
+export default class FckTextWebPart extends BaseClientSideWebPart<
+    IFckTextWebPartProps
+    > {
+    private guid: string;
 
-  private guid: string;
+    /**
+     * @function
+     * Web part contructor.
+     */
+    public constructor(context?: IWebPartContext) {
+        super();
 
-  /**
-   * @function
-   * Web part contructor.
-   */
-  public constructor(context?: IWebPartContext) {
-    super();
+        this.guid = this.getGuid();
 
-    this.guid = this.getGuid();
-
-    //Hack: to invoke correctly the onPropertyChange function outside this class
-    //we need to bind this object on it first
-    this.onPropertyPaneFieldChanged = this.onPropertyPaneFieldChanged.bind(this);
-  }
-
-  /**
-   * @function
-   * Gets WP data version
-   */
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
-
-  /**
-   * @function
-   * Renders HTML code
-   */
-  public render(): void {
-
-    if (Environment.type === EnvironmentType.ClassicSharePoint) {
-      var errorHtml = '';
-      errorHtml += '<div style="color: red;">';
-      errorHtml += '<div style="display:inline-block; vertical-align: middle;"><i class="ms-Icon ms-Icon--Error" style="font-size: 20px"></i></div>';
-      errorHtml += '<div style="display:inline-block; vertical-align: middle;margin-left:7px;"><span>';
-      errorHtml += strings.ErrorClassicSharePoint;
-      errorHtml += '</span></div>';
-      errorHtml += '</div>';
-      this.domElement.innerHTML = errorHtml;
-      return;
+        //Hack: to invoke correctly the onPropertyChange function outside this class
+        //we need to bind this object on it first
+        this.onPropertyPaneFieldChanged = this.onPropertyPaneFieldChanged.bind(
+            this
+        );
     }
 
+    /**
+     * @function
+     * Gets WP data version
+     */
+    protected get dataVersion(): Version {
+        return Version.parse("1.0");
+    }
 
-    if (this.displayMode == DisplayMode.Edit) {
-      //Edit mode
-      var html = '';
-      html += "<textarea name='" + this.guid + "-editor' id='" + this.guid + "-editor'>" + this.properties.text + "</textarea>";
-      this.domElement.innerHTML = html;
-
-      var ckEditorCdn: string = '//cdn.ckeditor.com/4.6.2/full/ckeditor.js';
-      SPComponentLoader.loadScript(ckEditorCdn, { globalExportsName: 'CKEDITOR' }).then((CKEDITOR: any): void => {
-        if (this.properties.inline == null || this.properties.inline === false)
-          CKEDITOR.replace( this.guid + '-editor', {
-              skin: 'moono-lisa,//cdn.ckeditor.com/4.6.2/full-all/skins/moono-lisa/'
-          }  );
-        else
-          CKEDITOR.inline( this.guid + '-editor', {
-              skin: 'moono-lisa,//cdn.ckeditor.com/4.6.2/full-all/skins/moono-lisa/'
-          }   );
-
-        for (var i in CKEDITOR.instances) {
-          CKEDITOR.instances[i].on('change', (elm?, val?) =>
-          {
-            //CKEDITOR.instances[i].updateElement();
-            elm.sender.updateElement();
-            var value = ((document.getElementById(this.guid + '-editor')) as any).value;
-            if (this.onPropertyPaneFieldChanged && value != null) {
-              this.properties.text = value;
-            }
-          });
+    /**
+     * @function
+     * Renders HTML code
+     */
+    public render(): void {
+        if (Environment.type === EnvironmentType.ClassicSharePoint) {
+            var errorHtml = "";
+            errorHtml += '<div style="color: red;">';
+            errorHtml +=
+                '<div style="display:inline-block; vertical-align: middle;"><i class="ms-Icon ms-Icon--Error" style="font-size: 20px"></i></div>';
+            errorHtml +=
+                '<div style="display:inline-block; vertical-align: middle;margin-left:7px;"><span>';
+            errorHtml += strings.ErrorClassicSharePoint;
+            errorHtml += "</span></div>";
+            errorHtml += "</div>";
+            this.domElement.innerHTML = errorHtml;
+            return;
         }
-      });
-    }
-    else {
-      //Read Mode
-      this.domElement.innerHTML = this.properties.text;
-    }
-  }
 
-  /**
-   * @function
-   * Generates a GUID
-   */
-  private getGuid(): string {
-    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' +
-      this.s4() + '-' + this.s4() + this.s4() + this.s4();
-  }
+        if (this.displayMode == DisplayMode.Edit) {
+            //Edit mode
+            var html = "";
+            html += "<style>.cke .cke_top {display: block !important;} </style>";
+            html +=
+                "<textarea name='" +
+                this.guid +
+                "-editor' id='" +
+                this.guid +
+                "-editor'>" +
+                this.properties.text +
+                "</textarea>";
+            this.domElement.innerHTML = html;
 
-  /**
-   * @function
-   * Generates a GUID part
-   */
-  private s4(): string {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-  }
+            var ckEditorCdn: string = "//cdn.ckeditor.com/4.15.0/full/ckeditor.js";
+            SPComponentLoader.loadScript(ckEditorCdn, {
+                globalExportsName: "CKEDITOR"
+            }).then((CKEDITOR: any): void => {
+                if (this.properties.inline == null || this.properties.inline === false)
+                    CKEDITOR.replace(this.guid + "-editor", {
+                        skin: 'moono-lisa,//cdn.ckeditor.com/4.15.0/full-all/skins/moono-lisa/'
+                    });
+                else
+                    //Disable CKEditor auto attaching inline to editable elements
+                    CKEDITOR.disableAutoInline = true;
 
-  /**
-   * @function
-   * PropertyPanel settings definition
-   */
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          displayGroupsAsAccordion: false,
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneToggle('inline', {
-                  label: strings.Inline,
-                })
-              ]
-            }
-          ]
+                    CKEDITOR.inline(this.guid + "-editor", {
+                        skin: 'moono-lisa,//cdn.ckeditor.com/4.15.0/full-all/skins/moono-lisa/'
+                    });
+
+                for (var i in CKEDITOR.instances) {
+                    CKEDITOR.instances[i].on("change", (elm?, val?) => {
+                        //CKEDITOR.instances[i].updateElement();
+                        elm.sender.updateElement();
+                        if (document.getElementById(this.guid + "-editor")) {
+                            var value = (document.getElementById(this.guid + "-editor") as any)
+                                .value;
+                            if (this.onPropertyPaneFieldChanged && value != null) {
+                                this.properties.text = value;
+                            }
+                        }
+                    });
+                }
+            });
+        } else {
+            //Read Mode
+            this.domElement.innerHTML = this.properties.text;
         }
-      ]
-    };
-  }
+    }
+
+    /**
+     * @function
+     * Generates a GUID
+     */
+    private getGuid(): string {
+        return (
+            this.s4() +
+            this.s4() +
+            "-" +
+            this.s4() +
+            "-" +
+            this.s4() +
+            "-" +
+            this.s4() +
+            "-" +
+            this.s4() +
+            this.s4() +
+            this.s4()
+        );
+    }
+
+    /**
+     * @function
+     * Generates a GUID part
+     */
+    private s4(): string {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    /**
+     * @function
+     * PropertyPanel settings definition
+     */
+    protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+        return {
+            pages: [
+                {
+                    header: {
+                        description: strings.PropertyPaneDescription
+                    },
+                    displayGroupsAsAccordion: false,
+                    groups: [
+                        {
+                            groupName: strings.BasicGroupName,
+                            groupFields: [
+                                PropertyPaneToggle("inline", {
+                                    label: strings.Inline
+                                })
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+    }
 }
